464d7b9f7e26f5bb1bb3fff952a819f9ec60606c
zhongda <zhongda@1024-tech.com>
2020-07-14 17:53:36 +0800

    some thing 阔怕

---
+++ MaybeBug/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type MyError struct {
+	Now time.Time
+}
+
+func (m *MyError) Error() string {
+	fmt.Println("--- test before ---")
+
+	// 这里会导致神奇的栈溢出
+	// 由于此时m为nil, 会在里面触发 m 的error, 从而导致循环调用 error...  阔怕~
+	str := fmt.Sprintf("error %v", m)
+
+	// str += " test"
+	fmt.Println("--- test after ---")
+
+	return str
+}
+
+func Get() error {
+	var m *MyError
+
+	m = &MyError{
+		time.Now(),
+	}
+	return m
+}
+
+func main() {
+
+	var m *MyError
+	str := fmt.Sprintf("hh %v", m)
+	fmt.Println(str)
+
+	return
+
+	/*
+	if e := Get(); e != nil {
+		fmt.Println(e)
+	}
+
+	 */
+}
